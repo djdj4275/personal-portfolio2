23cecb85616454c8b2cf607375807f559fbbd658
djdj4275 <djdj4275@naver.com>
2022-05-04 10:18:09 +0900

    'portfolio2'

---
+++ portfolio2/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app" :style="{'background' : background}">
+    <nav>
+      <router-link to="/" @click.native="checkhome = true">Home</router-link> |
+      <router-link to="/about" @click.native="checkhome = false">About</router-link> |
+      <router-link to="/page" @click.native="checkhome = false">Page</router-link> |
+      <router-link to="/test" @click.native="checkhome = false">Test</router-link>
+    </nav>
+
+    <router-view/>
+  </div>
+</template>
+
+<style scoped>
+  #app {
+    width : 100%;
+    height: 100%;
+    background-size: cover;
+  }
+</style>
+
+<script>
+export default {
+  data () {
+    return {
+      checkhome : true,
+    } 
+  },
+  computed : {
+    background() {
+    if (this.checkhome) {
+      return "url("+require("./assets/coffeebook.jpg")+")";
+    }
+    else {
+      return "#000000";
+    }
+    }
+  }
+}
+</script>
