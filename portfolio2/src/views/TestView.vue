fb5c615dfed83942df1507dadb3c3794cd08c72a
djdj4275 <djdj4275@naver.com>
2022-05-21 19:34:07 +0900

    수정18

--- portfolio2/src/views/TestView.vue
+++ portfolio2/src/views/TestView.vue
@@ -23,6 +23,20 @@
         <div v-if="mapopen" class="black-bg">
             <div class="white-bg">
                 <kakao-map :Lat="lat" :Lng="lng" class="kakao" />
+
+                <div class="slideShow">
+                    <button @click="moveleft" class="slideShow_left">&lang;</button>
+                    <div class="slideShow_itembox">
+                        <ul class="slideShow_items" ref="slideshow">
+                            <li><img class= "slideShow_item" src="../assets/1.jpg" /></li>
+                            <li><img class= "slideShow_item" src="../assets/2.jpg" /></li>
+                            <li><img class= "slideShow_item" src="../assets/3.jpg" /></li>
+                            <li><img class= "slideShow_item" src="../assets/4.jpg" /></li>
+                        </ul>
+                    </div>
+                    <button @click="moveright" class="slideShow_right">&rang;</button>
+                </div>
+
                 <button @click="mapopen=false" class="kakaooff">X</button>
             </div>
         </div>
@@ -33,6 +47,9 @@
 
 <script>
 import KakaoMap from '../components/KakaoMap.vue';
+
+    let currentIdx = 0; //현재 슬라이드 index 
+    const slideCount = 4; // 슬라이드 개수
 
 export default {
     components: { KakaoMap },
@@ -51,8 +68,22 @@ export default {
             this.mapopen=true;
             this.lat = Lat;
             this.lng = Lng;
-        }
-    }
+        },
+        moveSlide(num) {
+            this.$refs.slideshow.style.transform = `translateX(${-num * 200 + "px"})`;
+            console.log(this.$refs.slideshow.style.trasform);
+            currentIdx = num; 
+        },
+        moveleft() {
+            if (currentIdx !== 0) 
+                this.moveSlide(currentIdx - 1);
+        },
+        moveright() {
+            if (currentIdx !== slideCount - 1) {
+            this.moveSlide(currentIdx + 1);
+            }
+        },
+    },
 }
 </script>
 
